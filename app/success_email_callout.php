5259b1640d55d76949a698b45361237e67f21610
halldavidwv <hall.davidwv@gmail.com>
2024-02-15 19:59:49 +0800

    feat: option in prerequisite window
    Added select options for Phase 3 Step 1 Prerequisite Window for Tapping Point that will be installed in the service line. The purpose of the select option is for the expected days needed since the two options have different expected working days.

--- app/success_email_callout.php
+++
@@ -1,6 +0,0 @@
-<div class="callout success" data-closable="slide-out-right">
-    <h5>Email Sent Successfully!</h5>
-    <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
