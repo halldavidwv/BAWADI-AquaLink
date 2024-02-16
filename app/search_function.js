9710676ad2c049faa7bde3854c85efbb96312fdc
unknown <hall.davidwv@gmail.com>
2024-02-17 07:35:46 +0800

    fix: add search function js file

---
+++ app/search_function.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $('#tracking_number_search').keyup(function () { 
+    var search = $(this).val();
+    // Function to update the table when search value applied
+    function searchContent(url, searchData, tableID) {
+      $.ajax({
+        url: url,
+        method: 'POST',
+        data: { searchData: searchData },
+        success: function (data) {
+          $("#" + tableID).empty();
+          $("#" + tableID).html(data);
+          $("#" + tableID).html(data).foundation();
+        }
+      });
+    }
+
+    searchContent("main_table_search.php", search, "main-table");
+    searchContent("phase_2_step_4_complete_search.php", search, "phase-2-step-4-complete-table");
+
+  });
+
+  function updateContent(url, tableID) {
+    $.ajax({
+      url: url,
+      success: function (data) {
+        $("#" + tableID).html(data);
+        $("#" + tableID).html(data).foundation();
+      }
+    });
+  }
+
+  updateContent("main_table_search.php", "main-table");
+  updateContent("phase_2_step_4_complete_search.php", "phase-2-step-4-complete-table");
+
+});
