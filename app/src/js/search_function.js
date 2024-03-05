e84f1fd6e122877a8e8f95c5a9531b9b49f74e6b
halldavidwv <hall.davidwv@gmail.com>
2024-03-05 14:08:46 +0800

    feat: debounce function in search function
    Added debounce function in search function in order to not query every single keystroke in search box but after the user stopped typing will the search function work.

--- app/src/js/search_function.js
+++ app/src/js/search_function.js
@@ -1,25 +1,37 @@
 $(document).ready(function () {
-  $('#tracking_number_search').keyup(function () { 
-    var search = $(this).val();
-    // Function to update the table when search value applied
-    function searchContent(url, searchData, tableID) {
-      $.ajax({
-        url: url,
-        method: 'POST',
-        data: { searchData: searchData },
-        success: function (data) {
-          $("#" + tableID).empty();
-          $("#" + tableID).html(data);
-          $("#" + tableID).html(data).foundation();
-        }
-      });
-    }
 
+  const debounce = function (func, delay) {
+    let timer;
+    return function (...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  };
+
+  // Function to update the table when search value applied
+  function searchContent(url, searchData, tableID) {
+    $.ajax({
+      url: url,
+      method: 'POST',
+      data: { searchData: searchData },
+      success: function (data) {
+        $("#" + tableID).empty();
+        $("#" + tableID).html(data);
+        $("#" + tableID).html(data).foundation();
+      }
+    });
+  }
+
+  const debounceSearch = debounce(function () {
+    let search = $("#tracking_number_search").val();
     searchContent("src/php/main_table_search.php", search, "main-table");
     searchContent("src/php/phase_2_step_4_complete_search.php", search, "phase-2-step-4-complete-table");
     searchContent("src/php/archive_table_search.php", search, "archive-table");
+  }, 300);
 
-  });
+  $('#tracking_number_search').keyup(debounceSearch);
 
   function updateContent(url, tableID) {
     $.ajax({
