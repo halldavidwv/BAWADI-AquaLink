54eb2f1274ca2380ef09bd88e9723a0c1358e10c
unknown <hall.davidwv@gmail.com>
2024-05-11 11:49:27 +0800

    refactor: fix address in phase 2 step 4 complete
    Fixed the displaying of address in Phase 2 Step 4 Complete.

--- app/src/php/tables/display_table_function.php
+++ app/src/php/tables/display_table_function.php
@@ -1,7 +1,8 @@
 <?php
 
-    function display_table_header() {
-        echo "<thead><tr>
+function display_table_header()
+{
+  echo "<thead><tr>
                 <th>Tracking Number</th>
                 <th>Customer Name</th>
                 <th>Home Address</th>
@@ -11,34 +12,35 @@
                 <th>Action</th>
               </tr>
             </thead>";
-    }
+}
 
-    function display_table_body($row) {
-      echo "<tr>";
-        echo "<td>" 
-                . $row['tracking_number'] . 
-            "</td>";
-        echo "<td>"
-                . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .   
-            "</td>";
-        echo "<td>"
-                . $row['home_address_street'] . ", " . $row['home_address_city'] . 
-            "</td>";
-        echo "<td>"
-                . $row['email_address'] .
-            "</td>";
-        echo "<td>"
-                . $row['step'] . " 
+function display_table_body($row)
+{
+  echo "<tr>";
+  echo "<td>"
+    . $row['tracking_number'] .
+    "</td>";
+  echo "<td>"
+    . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .
+    "</td>";
+  echo "<td>"
+    . $row['home_address_street'] . ", " . $row['home_address_city'] .
+    "</td>";
+  echo "<td>"
+    . $row['email_address'] .
+    "</td>";
+  echo "<td>"
+    . $row['step'] . " 
               <a id='step-button-" . $row['id'] . "' data-open='step_details_window' data-value='" . $row['step'] . "'>
                 <i class='fa-solid fa-circle-info fa-2xl'></i>
               </a>
               </td>";
-        echo "<td>" 
-                . $row['time_updated'] . 
-             "</td>";
-        echo "<td>
+  echo "<td>"
+    . $row['time_updated'] .
+    "</td>";
+  echo "<td>
                 <div class='tiny button-group align-center-middle'>
-                  <a id='next-step-button-" . $row['id'] . "' class='button' data-value='" . $row['step'] . "' data-id='". $row['id'] . "'>
+                  <a id='next-step-button-" . $row['id'] . "' class='button' data-value='" . $row['step'] . "' data-id='" . $row['id'] . "'>
                     <i class='fa-solid fa-forward-step fa-2xl'></i>
                   </a>
                   <a href='src/php/edit_button.php?id=" . $row['id'] . "' class='button'>
@@ -50,37 +52,38 @@
                 </div>
               </td>
             </tr>";
-    }
+}
 
-    function display_table_body_with_email($row) {
-      echo "<tr>";
-        echo "<td>" 
-                . $row['tracking_number'] . 
-            "</td>";
-        echo "<td>"
-                . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .
-            "</td>";
-        echo "<td>"
-                . $row['home_address'] . 
-            "</td>";
-        echo "<td>"
-                . $row['email_address'] .
-            "</td>";
-        echo "<td>"
-                . $row['step'] . " 
+function display_table_body_with_email($row)
+{
+  echo "<tr>";
+  echo "<td>"
+    . $row['tracking_number'] .
+    "</td>";
+  echo "<td>"
+    . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .
+    "</td>";
+  echo "<td>"
+    . $row['home_address_street'] . ", " . $row['home_address_city'] .
+    "</td>";
+  echo "<td>"
+    . $row['email_address'] .
+    "</td>";
+  echo "<td>"
+    . $row['step'] . " 
               <a id='step-button-" . $row['id'] . "' data-open='step_details_window' data-value='" . $row['step'] . "'>
                 <i class='fa-solid fa-circle-info fa-2xl'></i>
               </a>
               </td>";
-        echo "<td>" 
-                . $row['time_updated'] . 
-             "</td>";
-        echo "<td>
+  echo "<td>"
+    . $row['time_updated'] .
+    "</td>";
+  echo "<td>
                 <div class='tiny button-group align-center-middle'>
-                  <a id='email-window-button-" . $row['id'] . "' class='button' data-open='email_window' data-id='". $row['id'] . "'>
+                  <a id='email-window-button-" . $row['id'] . "' class='button' data-open='email_window' data-id='" . $row['id'] . "'>
                     <i class='fa-solid fa-envelope fa-2xl'></i>
                   </a>
-                  <a id='next-step-button-" . $row['id'] . "' class='button' data-value='" . $row['step'] . "' data-id='". $row['id'] . "'>
+                  <a id='next-step-button-" . $row['id'] . "' class='button' data-value='" . $row['step'] . "' data-id='" . $row['id'] . "'>
                     <i class='fa-solid fa-forward-step fa-2xl'></i>
                   </a>
                   <a href='src/php/edit_button.php?id=" . $row['id'] . "' class='button'>
@@ -92,32 +95,33 @@
                 </div>
               </td>
             </tr>";
-    }
+}
 
-    function display_table_body_completed($row) {
-      echo "<tr>";
-        echo "<td>" 
-                . $row['tracking_number'] . 
-            "</td>";
-        echo "<td>"
-                . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .
-            "</td>";
-        echo "<td>"
-                . $row['home_address'] . 
-            "</td>";
-        echo "<td>"
-                . $row['email_address'] .
-            "</td>";
-        echo "<td>"
-                . $row['step'] . " 
+function display_table_body_completed($row)
+{
+  echo "<tr>";
+  echo "<td>"
+    . $row['tracking_number'] .
+    "</td>";
+  echo "<td>"
+    . $row['last_name'] . ", " . $row['first_name'] . " " . $row['middle_name'] .
+    "</td>";
+  echo "<td>"
+    . $row['home_address'] .
+    "</td>";
+  echo "<td>"
+    . $row['email_address'] .
+    "</td>";
+  echo "<td>"
+    . $row['step'] . " 
               <a id='step-button-" . $row['id'] . "' data-open='step_details_window' data-value='" . $row['step'] . "'>
                 <i class='fa-solid fa-circle-info fa-2xl'></i>
               </a>
               </td>";
-        echo "<td>" 
-                . $row['time_updated'] . 
-             "</td>";
-        echo "<td>
+  echo "<td>"
+    . $row['time_updated'] .
+    "</td>";
+  echo "<td>
                 <div class='tiny button-group align-center-middle'>
                   <a href='src/php/edit_button.php?id=" . $row['id'] . "' class='button'>
                     <i class='fa-regular fa-pen-to-square fa-2xl'></i>
@@ -128,6 +132,4 @@
                 </div>
               </td>
             </tr>";
-    }
-
-?>
+}
